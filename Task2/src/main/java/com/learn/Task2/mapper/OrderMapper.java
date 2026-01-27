6dd189f353c5492cd1e4db0933223642159fff32
Anindya Basak <anindyabasak.eteruet@gmail.com>
2026-01-28 01:44:05 +0600

    in memory db implementation

--- Task2/src/main/java/com/learn/Task2/mapper/OrderMapper.java
+++ Task2/src/main/java/com/learn/Task2/mapper/OrderMapper.java
@@ -1,4 +1,20 @@
 package com.learn.Task2.mapper;
 
+
+import com.learn.Task2.model.dto.request.CreateOrderRequest;
+import com.learn.Task2.model.dto.response.OrderResponse;
+import com.learn.Task2.model.entity.Order;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
 public interface OrderMapper {
+
+    Order toEntity(CreateOrderRequest order);
+    OrderResponse toDto(Order order);
+
+
 }
--- Task2/src/main/java/com/learn/Task2/repository/impl/InMemoryOrderRepository.java
+++ Task2/src/main/java/com/learn/Task2/repository/impl/InMemoryOrderRepository.java
@@ -1,10 +1,40 @@
 package com.learn.Task2.repository.impl;
 
+import com.learn.Task2.model.entity.Order;
 import com.learn.Task2.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Repository
 @Slf4j
 public class InMemoryOrderRepository implements OrderRepository {
+
+
+    private final Map<Long, Order> store = new ConcurrentHashMap<>();
+
+    private final AtomicLong idGenerator = new AtomicLong(0);
+
+    public Order save(Order order){
+
+        if (order.getId()== null){
+            order.setId(idGenerator.getAndIncrement());
+            order.setCreatedAt(LocalDateTime.now());
+        }
+
+        order.setUpdatedAt(LocalDateTime.now());
+        store.put(order.getId(), order);
+        return order;
+    }
+
+    public Optional<Order> findById(Long id){
+        return Optional.ofNullable(store.get(id));
+    }
+
 }
+
