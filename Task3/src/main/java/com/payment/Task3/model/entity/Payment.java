eeeadeafbc6ffa7fb560c2bf7b73f485b3e8e663
Anindya Basak <anindyabasak.eteruet@gmail.com>
2026-01-28 17:31:02 +0600

    payment entity implementation

--- Task3/src/main/java/com/payment/Task3/model/entity/Payment.java
+++ Task3/src/main/java/com/payment/Task3/model/entity/Payment.java
@@ -1,11 +1,18 @@
 package com.payment.Task3.model.entity;
 
 import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
 
     private Long id;
@@ -16,7 +23,11 @@ public class Payment {
 
     private PaymentStatus status;
 
+    private String externalReference;
 
+    private String IdempotencyKey;
 
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
 
 }
